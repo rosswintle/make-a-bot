d84bdfa52351b49d08fc105cf4af9e617eaa55c4
Ross Wintle <ross@oikos.org.uk>
2017-09-17 21:31:27 +0100

    Attempt at adding Get Started response

--- routes/botman.php
+++ routes/botman.php
@@ -3,6 +3,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
+// This is the get started text
+$botman->hears('GET_STARTED', function ($bot) {
+	$bot->reply('Hi. I can\'t do much right now, but try saying "Hi", or asking me to "Start conversation"');
+});
+
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
